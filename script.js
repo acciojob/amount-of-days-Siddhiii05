46fdea9ab5613130a7e2a03501392198d691f94a
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-13 19:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let numOfDays = 365;
+	let leapYear = 366;
+
+	if (numOfDays >= 365){
+		return year;
+	}
+	if (leapYear >= 366){
+		return year;
+	}
+}
+
+daysOfAYear(2022);
